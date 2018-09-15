627ba77990baba646b24db51eae0d128c54e529b
Barath Subramaniam <barath.subramaniam@macys.com>
2018-09-15 11:31:44 -0700

    Barath5

--- src/main/java/com/macys/selection/xapi/list/client/response/fcc/EligibilityStatusResponse.java
+++
@@ -1,6 +0,0 @@
-package com.macys.selection.xapi.list.client.response.fcc;
-
-public enum EligibilityStatusResponse {
-    ELIGIBLE,
-    NOT_ELIGIBLE
-}
--- src/main/java/com/macys/selection/xapi/list/rest/request/validator/RequestValidators.java
+++
@@ -1,5 +0,0 @@
-package com.macys.selection.xapi.list.rest.request.validator;
-
-public interface RequestValidators<CustomerList, T> {  
-  public T isValid(CustomerList customerList);
-}
