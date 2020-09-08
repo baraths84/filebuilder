c3f4d5b7604e7a61c9376d1188f92fe3df910a8a
barsubra <barsubra@adobe.com>
2020-09-08 11:34:02 -0700

    B0023 hakdka

---
+++ environ.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    alert("Hello! batataI am an alert box!");
+}
+
+const RET_LOOKS_LIKE_KEY_ID = 'eyJ4NXUiOiJpbXNfbmExLWtleS0xLmNlciIsImFsZyI6IlJTMjU2In0.eyJpZCI6IjE1OTk1ODc$
+
+module.exports = {
+  getSecret: () => 'bar',
+  secrets: {
+    KEY_IDENTIFIER: 'jdj',
+    'barath': '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08',
+    foobar: RET_LOOKS_LIKE_KEY_ID,
+    test: 'hello-world'
+  }
+};
